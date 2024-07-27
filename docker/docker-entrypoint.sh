39f6a8202a5d940189d1fa317069304d2520cadb
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-27 14:29:02 +0900

    dpoコード追加

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if [ ! -d "/tmp/own/docker/entrypoint/check" ]; then
     mkdir /tmp/own/docker/entrypoint/check
     echo 'export HF_HUB_CACHE=/home/docker/LLaVA-JP/cache' >> ~/.bashrc
+    cd ./external_libs/trl && pip install -e . && cd ../../
     exec "$@"
 else
     exec "$@"
